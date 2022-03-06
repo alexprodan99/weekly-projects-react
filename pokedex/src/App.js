b5ff57ba52b7e86df3650d7ffd11880ceb6718ad
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-03-06 16:32:23 +0200

    some updates on routes

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import PokemonDetail from './components/PokemonDetail';
 import PokemonPage from './components/PokemonPage';
 
 function App() {
   
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<PokemonPage />}></Route>
-          <Route path="/home" element={<PokemonPage />}></Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/">
+          <Route path="" element={<PokemonPage />}></Route>
+          <Route path="home" element={<PokemonPage />}></Route>
+          <Route path="pokemons">
+            <Route path="" element={<PokemonPage />}></Route>
+            <Route path=":name" element={<PokemonDetail />}></Route>
+          </Route>  
+        </Route>
+        
+        <Route path="*" element={<main style={{ padding: "1rem" }}><p>Page Not Found!</p></main>}></Route>
+      </Routes>
     </div>
   );
 }
