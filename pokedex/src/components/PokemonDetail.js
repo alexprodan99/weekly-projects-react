d6de07b428fcb09ff5b9af4b14d17675fd81ba22
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-03-07 21:05:40 +0200

    get current details page pokemon name

--- pokedex/src/components/PokemonDetail.js
+++ pokedex/src/components/PokemonDetail.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function PokemonDetail() {
+  const { name } = useParams();
+
   return (
-    <div>PokemonDetail</div>
+    <div> { name } </div>
   )
 }
