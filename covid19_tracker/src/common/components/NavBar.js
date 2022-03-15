06840a829cf35dceedf45d80813a9f115fe79f49
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-03-15 22:24:55 +0200

    align navbar items to right

--- covid19_tracker/src/common/components/NavBar.js
+++ covid19_tracker/src/common/components/NavBar.js
@@ -9,7 +9,7 @@ export default function NavBar() {
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
+            <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                     <Link className="nav-link" to="/">WorldWide</Link>
                 </li>
