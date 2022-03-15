e15dc3873382ed80c6ec41994eccce0943fbce46
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-03-15 21:28:20 +0200

    basic axios api

--- covid19_tracker/src/App.js
+++ covid19_tracker/src/App.js
@@ -1,12 +1,13 @@
+import React, { useEffect } from 'react'
 import './App.css'
 import BarChart from './common/components/BarChart'
+
 
 function App() {
     const data = [
         { x_axis: 'baba', y_axis: 20, color: 'green' },
         { x_axis: 'caca', y_axis: 30, color: 'yellow' },
     ]
-
     return (
         <div>
             <BarChart width={600} height={400} data={data} />
