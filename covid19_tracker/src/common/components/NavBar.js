9a0f9a71647aa7db03144bfd7beb9025682c93f6
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-03-15 22:19:13 +0200

    some changes on navbar

--- covid19_tracker/src/common/components/NavBar.js
+++ covid19_tracker/src/common/components/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <nav className="navbar navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <Link className="navbar-brand" to="/">Covid19Tracker</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -11,7 +11,13 @@ export default function NavBar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                    <Link className="nav-link" to="#">Link</Link>
+                    <Link className="nav-link" to="/">WorldWide</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/country">By country</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/what-to-do">What to do</Link>
                 </li>
             </ul>
         </div>
