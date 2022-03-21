8f0ffafc020cc5d6afd596f32fe141887c8004f7
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-03-21 19:49:50 +0200

    create an api client and use it for creating my news api

--- hacker_news_feed/src/App.js
+++ hacker_news_feed/src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { getSearchResults } from './api/news-api';
 
 function App() {
+    useEffect(() => {
+        getSearchResults('foo').then((results) => {
+            console.log(results);
+        });
+    }, []);
     return <div></div>;
 }
 
