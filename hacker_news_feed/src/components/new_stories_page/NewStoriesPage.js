e8f3bc68fb6a2913ae5e9a777189fbc2dc187d7c
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-03-24 20:42:08 +0200

    add styling for pagination component

--- hacker_news_feed/src/components/new_stories_page/NewStoriesPage.js
+++ hacker_news_feed/src/components/new_stories_page/NewStoriesPage.js
@@ -55,6 +55,16 @@ export default function NewStoriesPage() {
                 pageRangeDisplayed={5}
                 pageCount={pageCount}
                 previousLabel="< previous"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
                 renderOnZeroPageCount={null}
             />
         </div>
--- hacker_news_feed/src/components/show_stories_page/ShowStoriesPage.js
+++ hacker_news_feed/src/components/show_stories_page/ShowStoriesPage.js
@@ -57,6 +57,16 @@ export default function ShowStoriesPage() {
                 pageRangeDisplayed={5}
                 pageCount={pageCount}
                 previousLabel="< previous"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
                 renderOnZeroPageCount={null}
             />
         </div>
