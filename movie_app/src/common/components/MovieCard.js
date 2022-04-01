6fc5f29d081984f7abe2e6490a4624183b72a7d9
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-04-01 16:11:35 +0300

    add vote average and genre badge

--- movie_app/src/common/components/MovieCard.js
+++ movie_app/src/common/components/MovieCard.js
@@ -5,7 +5,7 @@ export default function MovieCard({ title, genre, posterPath, voteAverage }) {
     return (
         <div
             className="card hover-zoomin"
-            style={{ width: '200px', height: '430px', marginBottom: '20px' }}
+            style={{ width: '200px', height: '450px', marginBottom: '20px' }}
         >
             <img
                 src={`https://image.tmdb.org/t/p/w1280/${posterPath}`}
@@ -14,7 +14,15 @@ export default function MovieCard({ title, genre, posterPath, voteAverage }) {
             />
             <div className="card-body">
                 <h5 className="card-text"> {title}</h5>
-                <h6 className="card-text"> {genre}</h6>
+                <h6 className="card-text">
+                    {' '}
+                    <span className="badge badge-secondary">{genre}</span>
+                </h6>
+                <h6 className="card-text">
+                    {' '}
+                    <i className="bi bi-star"></i>
+                    {voteAverage}
+                </h6>
             </div>
         </div>
     );
