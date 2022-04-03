b77b5245e09423fe0fb06668b6d46ab14bd78d53
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-04-03 09:14:42 +0300

    delete unnecessary app.css file

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard/Dashboard';
 import MovieDetails from './common/components/MovieDetails';
