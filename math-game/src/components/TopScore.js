e0e53ae78c5fea85baa4d7f82d93b6729b187319
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-04-14 21:05:05 +0300

    add top score page

--- math-game/src/components/TopScore.js
+++ math-game/src/components/TopScore.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function TopScore() {
-    return <div>TopScore</div>;
+    const [scoreList, setScoreList] = useState([]);
+    const [errorMsg, setErrorMsg] = useState('');
+    useEffect(() => {
+        fetch('http://localhost:8000/highscore', {
+            method: 'GET',
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                setScoreList(result);
+                setErrorMsg('');
+            })
+            .catch((error) => {
+                setErrorMsg(error.message);
+            });
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="top-score-wrapper">
+                {errorMsg && (
+                    <div className="alert alert-danger">{errorMsg}</div>
+                )}
+                <h2>HighScore</h2>
+                {scoreList.map((item, index) => {
+                    return (
+                        <div key={index} className="score-item">
+                            {index + 1}. {item.username} - {item.score} points
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default TopScore;
