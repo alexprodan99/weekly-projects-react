369ea1feec434b621e858c246d122c30fdb0ccf8
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-05-01 09:38:19 +0300

    add not found page

--- quiz-app/src/components/NotFoundPage.jsx
+++ quiz-app/src/components/NotFoundPage.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="not-found-page">
+      <h2>Oops! Page not found.</h2>
+      <h1>404</h1>
+      <p>We can't find the page you're looking for.</p>
+      <a onClick={() => navigate('/')}>Go back to dashboard</a>
+    </div>
+  );
 }
