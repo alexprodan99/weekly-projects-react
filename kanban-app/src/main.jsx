0cf5b5f68abd4e6329186c8ae0b5691b05304bd9
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-05-02 19:04:29 +0300

    kanban app initial commit - basic boilerplate structure

---
+++ kanban-app/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './index.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import getStorePersistor from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+
+import App from './App';
+
+const { store, persistor } = getStorePersistor();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
---
+++ kanban-app/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // default=localstorage
+import rootReducer from '../reducers';
+
+const persistConfig = {
+  key: 'kanban-app',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  const store = createStore(persistedReducer);
+  const persistor = persistStore(store);
+  window.store = store;
+  return { store, persistor };
+};
